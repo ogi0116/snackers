c84492a89b4a0d0d3fe0773893eafe8f09a84c4e
ogi0116 <135587780+ogi0116@users.noreply.github.com>
2023-10-20 04:27:27 +0000

    通知機能/モダールの修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,5 +7,8 @@ class Public::HomesController < ApplicationController
   #企業ステータスに設定したユーザーを一覧表示 User.companyでも可能
   def about
     @users = User.where(position_status: :company).order("created_at DESC").page(params[:page])
+    @notifications = current_user.notifications.order(created_at: :desc)
+    @unchecked_notifications = @notifications.where(checked: false).order(created_at: :desc)
+    @checked_notifications = @notifications.where(checked: true).order(created_at: :desc)
   end
 end
